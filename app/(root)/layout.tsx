c8f642296ccfc2fa54dfa6994951c396f9f22ac8
Sayan-97 <anistar71@gmail.com>
2024-11-27 03:49:40 +0530

    chore: updated metatags

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,6 +7,25 @@ import Footer from "@/components/shared/footer";
 export const metadata: Metadata = {
   title: "Aniket Banerjee",
   description: "My personal portfolio website.",
+  keywords: ["Aniket Banerjee", "Portfolio", "UI/UX", "Designer"],
+  authors: [
+    { name: "Aniket Banerjee", url: "https://aniketbanerjee.vercel.app/" },
+  ],
+  viewport: "width=device-width, initial-scale=1.0",
+  openGraph: {
+    title: "Aniket Banerjee",
+    description: "My personal portfolio website.",
+    url: "https://aniketbanerjee.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://cdn.sanity.io/images/70w14hnc/production/1a4f92603371ab3f4d39ce76a990807faff0f172-2453x1637.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Aniket Banerjee",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -15,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" dir="ltr" suppressHydrationWarning>
       <body
         className={`${fontOutfit.variable} ${fontInstrumentSerif.variable} antialiased`}
       >
