6174c2454fc443e42bf77936b1886a8fa5b20564
Sayan-97 <anistar71@gmail.com>
2024-11-26 22:13:11 +0530

    feat: caching

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -12,19 +12,18 @@ export const client = createClient({
 export async function sanityFetch<const QueryString extends string>({
   query,
   params = {},
-  revalidate = 86400, // default revalidation time in seconds
+  revalidate = 86400,
   tags = [],
 }: {
   query: QueryString;
   params?: QueryParams;
   revalidate?: number | false;
-  tags?: string[];
+  tags: string[];
 }) {
-  return client.fetch(query, params, {
-    cache: "force-cache",
+  return await client.fetch(query, params, {
     next: {
-      revalidate: tags.length ? false : revalidate, // for simple, time-based revalidation
-      tags, // for tag-based revalidation
+      revalidate: tags.length ? false : revalidate,
+      tags,
     },
   });
 }
